c2f5e8d3b27fe49fdd62138d929f86826da3da87
VieiraLaris <lalasarandi@gmail.com>
2025-10-02 20:52:36 -0300

    feat: initialize frontend with React, Vite, and TypeScript

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,20 @@
+const Home = () => {
+    return (
+      <div className="home-page-container">
+        <div className="content-wrapper">
+          <div className="title">
+            <h1>Home</h1>
+          </div>
+          <p className="description">
+            Processador de pedidos e frete.
+          </p>
+          <button className="button">
+            Clique aqui
+          </button>
+        </div>
+        
+      </div>
+    )
+  }
+  
+  export default Home
