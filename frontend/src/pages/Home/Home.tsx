e183c6fe6e59a97a9ddfd59ee1ec0c10496e16ea
VieiraLaris <lalasarandi@gmail.com>
2025-10-04 02:19:13 -0300

    update: atualiza página Home com header e catalogo de produtos

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,20 +1,37 @@
-const Home = () => {
-    return (
-      <div className="home-page-container">
-        <div className="content-wrapper">
-          <div className="title">
-            <h1>Home</h1>
-          </div>
-          <p className="description">
-            Processador de pedidos e frete.
-          </p>
-          <button className="button">
-            Clique aqui
-          </button>
+import styles from "./Home.module.css";
+import ProductGrid from "../../components/ProductGrid";
+import logo from "../../assets/images/online-shopping.png";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  description?: string;
+  stock: number;
+}
+
+interface HomeProps {
+  products: Product[];
+}
+
+const Home = ({ products }: HomeProps) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" width={40} />
+          comprAqui
         </div>
-        
+      </header>
+
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <ProductGrid products={products} />
+        </div>
       </div>
-    )
-  }
-  
-  export default Home
+    </>
+  );
+};
+
+export default Home;
